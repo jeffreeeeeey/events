faacf8dcef10fbc0ab6c2209ba747c42964e3ebd
jeffreeeeeey <jeffreeeeeey@gmail.com>
2015-05-06 16:14:31 +0800

    get topics for event

--- Events/Events/DiscoverViewController.h
+++ Events/Events/DiscoverViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DiscoverViewController : UITableViewController
+@interface DiscoverViewController : UIViewController
 
 
 @end
--- Events/Events/DiscoverViewController.m
+++ Events/Events/DiscoverViewController.m
@@ -8,7 +8,9 @@
 
 #import "DiscoverViewController.h"
 
-@interface DiscoverViewController ()
+@interface DiscoverViewController () <NSURLSessionDataDelegate>
+
+@property NSDictionary *jsonDic;
 
 @end
 
@@ -17,6 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self getTopics];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,6 +42,31 @@
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"topicCell" forIndexPath:indexPath];
     return cell;
+}
+
+#pragma mark - Network
+
+- (void)getTopics
+{
+    NSURLSessionConfiguration *defaultConfigObject = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *defaultSession = [NSURLSession sessionWithConfiguration:defaultConfigObject delegate:self delegateQueue:[NSOperationQueue mainQueue]];
+    
+    NSURL *url = [NSURL URLWithString:@"http://mpc.issll.com/llzgmri/m/p/topic/getPlotTopicsByType?type=2&page=1&long=116.501426&lat=39.921523"];
+    
+    NSURLSessionDataTask *dataTask = [defaultSession dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error == nil) {
+            NSString *text = [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+            //NSLog(@"Data = %@", text);
+            if ([NSJSONSerialization isValidJSONObject:text]) {
+                self.jsonDic = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                NSLog(@"json:%@", self.jsonDic);
+            } else {
+                NSLog(@"not valid");
+            }
+            
+        }
+    }];
+    [dataTask resume];
 }
 
 @end
